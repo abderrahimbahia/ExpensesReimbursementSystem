2ac557b1ed773ee7537486b1a4de8543816d4d68
Abderrahim Bahia <52228409+abderrahimbahia@users.noreply.github.com>
2022-09-04 16:36:42 -0400

    Delete src directory

--- src/main/java/exceptions/InvalidCredintialsException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InvalidCredintialsException extends Exception{
-
-    public InvalidCredintialsException(String message)
-    {
-        super(message);
-    }
-}
